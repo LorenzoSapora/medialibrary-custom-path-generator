d51276eaa47ba7b17d0e37c478dd0430e4c5e2b8
Lorenzo Sapora <lorenzo.sapora@gmail.com>
2020-06-08 03:19:35 +0100

    Fix path generation to be more inline with v8

--- src/CollectionnamePathGenerator.php
+++ src/CollectionnamePathGenerator.php
@@ -1,8 +1,7 @@
 <?php
-namespace Terminalsio\MedialibraryCustomPathGenerator;
+namespace Spatie\MediaLibrary\Support\PathGenerator;
 
-use Spatie\MediaLibrary\Models\Media;
-use Spatie\MediaLibrary\PathGenerator\PathGenerator;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class CustomPathGenerator implements PathGenerator
 {
@@ -18,4 +17,49 @@ class CustomPathGenerator implements PathGenerator
     {
         return $this->getPath($media).'/responsive-images/';
     }
+    protected function getBasePath(Media $media): string
+    {
+        return $media->getKey();
+    }
+
 }
+// <?php
+
+// namespace Spatie\MediaLibrary\Support\PathGenerator;
+
+// use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+// class DefaultPathGenerator implements PathGenerator
+// {
+//     /*
+//      * Get the path for the given media, relative to the root storage path.
+//      */
+//     public function getPath(Media $media): string
+//     {
+//         return $this->getBasePath($media).'/';
+//     }
+
+//     /*
+//      * Get the path for conversions of the given media, relative to the root storage path.
+//      */
+//     public function getPathForConversions(Media $media): string
+//     {
+//         return $this->getBasePath($media).'/conversions/';
+//     }
+
+//     /*
+//      * Get the path for responsive images of the given media, relative to the root storage path.
+//      */
+//     public function getPathForResponsiveImages(Media $media): string
+//     {
+//         return $this->getBasePath($media).'/responsive-images/';
+//     }
+
+//     /*
+//      * Get a unique base path for the given media.
+//      */
+//     protected function getBasePath(Media $media): string
+//     {
+//         return $media->getKey();
+//     }
+// }
